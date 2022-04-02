fa0f0955fd89d136fab437e2ec34766f5f2c0cc5
Nick Anderson <nicasioca@gmail.com>
2022-04-02 12:45:08 -0700

    clean up dynamic programming problem

--- 062_unique_paths.py
+++ 062_unique_paths.py
@@ -17,14 +17,12 @@ class Solution:
                 l = u = 0
 
                 # look back by one row to get value
-                if i-1 >= 0:
-                    u = dmap[i-1][j]
+                u = dmap[i-1][j]
                 
                 # look back by one column to get value
-                if j-1 >= 0:
-                    l = dmap[i][j-1]
+                l = dmap[i][j-1]
                 
-                # aggregate the values to get the total unique paths at this point
+                # aggregate the values to get the total unique paths to this point
                 dmap[i][j] = l + u
 
         # return the final number of unique paths 
@@ -37,5 +35,7 @@ if __name__ == '__main__':
     # robot starts top-left and must end in bottom-right corner
     # robot can only move either down or right at any point in time
     s = Solution()
+    # print(s.unique_path(2, 3))
+    # Output: 3
     print(s.unique_path(3, 7))
     # Output: 28
