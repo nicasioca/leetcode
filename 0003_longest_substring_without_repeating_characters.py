5c9351398dc792503a52bbe0074201815f213bc6
Nick Anderson <nicasioca@gmail.com>
2022-04-06 22:29:36 -0700

    Update 0003_longest_substring_without_repeating_characters.py

--- 0003_longest_substring_without_repeating_characters.py
+++ 0003_longest_substring_without_repeating_characters.py
@@ -1,15 +1,30 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen = {}
-        mx = left = 0
-        for right, c in enumerate(s):
-            if c in seen:
-                left = max(left, seen[c] + 1)
-            seen[c] = right
-            mx = max(mx, right-left+1)
-        return mx
+        dic, res, start, = {}, 0, 0
+        for i, ch in enumerate(s):
+            if ch in dic:
+                res = max(res, i-start) # update the res
+                start = max(start, dic[ch]+1)  # here should be careful, like "abba"
+            dic[ch] = i
+        return max(res, len(s)-start)  # return should consider the last non-repeated substring
 
 if __name__ == '__main__':
     s = Solution()
     print(s.lengthOfLongestSubstring('abcdefghijj'))
     # Output: 10
+
+# Example 1:
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+
+# Example 2:
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+
+# Example 3:
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
