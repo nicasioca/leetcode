4fe1897f63d3e8b63da0000178dbf8a85ea3c64b
Nick Anderson <nicasioca@gmail.com>
2022-04-24 21:04:38 -0700

    fix same tree problem solutions

--- 0100_same_tree.py
+++ 0100_same_tree.py
@@ -1,5 +1,3 @@
-# The "proper" way:
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -10,17 +8,40 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if p and q:
             return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        return p is q
+        else:
+            return p == q
 
-
-# The "tupleify" way:
-
-# def isSameTree(self, p, q):
-#     def t(n):
-#         return n and (n.val, t(n.left), t(n.right))
-#     return t(p) == t(q)
-
-
+# DFS with stack        
+# def isSameTree2(self, p, q):
+#     stack = [(p, q)]
+#     while stack:
+#         node1, node2 = stack.pop()
+#         if not node1 and not node2:
+#             continue
+#         elif None in [node1, node2]:
+#             return False
+#         else:
+#             if node1.val != node2.val:
+#                 return False
+#             stack.append((node1.right, node2.right))
+#             stack.append((node1.left, node2.left))
+#     return True
+ 
+# BFS with queue    
+# def isSameTree3(self, p, q):
+#     queue = [(p, q)]
+#     while queue:
+#         node1, node2 = queue.pop(0)
+#         if not node1 and not node2:
+#             continue
+#         elif None in [node1, node2]:
+#             return False
+#         else:
+#             if node1.val != node2.val:
+#                 return False
+#             queue.append((node1.left, node2.left))
+#             queue.append((node1.right, node2.right))
+#     return True
 
 
 # Example 1:
