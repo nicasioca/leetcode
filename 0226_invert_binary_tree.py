7243bfcfa4e010982fd3606b831023ce1fcffeec
Nick Anderson <nicasioca@gmail.com>
2022-04-24 21:45:10 -0700

    solve invert binary tree

---
+++ 0226_invert_binary_tree.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# class Solution:
+#     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+#         if root:
+#             root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+#         return root
+
+# # DFS
+def invertTree(self, root):
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        if node:
+            node.left, node.right = node.right, node.left
+            stack.extend([node.right, node.left])
+    return root
+
+# # BFS
+# def invertTree2(self, root):
+#     queue = collections.deque([(root)])
+#     while queue:
+#         node = queue.popleft()
+#         if node:
+#             node.left, node.right = node.right, node.left
+#             queue.append(node.left)
+#             queue.append(node.right)
+#     return root
+
+
+# Example 1:
+# Input: root = [4,2,7,1,3,6,9]
+# Output: [4,7,2,9,6,3,1]
+
+# Example 2:
+# Input: root = [2,1,3]
+# Output: [2,3,1]
+
+# Example 3:
+# Input: root = []
+# Output: []
