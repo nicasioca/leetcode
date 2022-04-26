d83cfa621983dc7bd5924ce6a0c372a1e9cd32e7
Nick Anderson <nicasioca@gmail.com>
2022-04-25 22:42:27 -0700

    add another tree search in a binary tree kth value down

--- 0230_kth_smallest_element_in_a_bst.py
+++ 0230_kth_smallest_element_in_a_bst.py
@@ -6,4 +6,41 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
+
+
+# # Recusive
+# class Solution:
+#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+#         self.k = k
+#         self.res = None
+#         self.helper(root)
+#         return self.res
+
+#     def helper(self, node):
+#         if node == None:
+#             return
+#         self.helper(node.left)
+#         self.k -= 1
+#         if self.k == 0:
+#             self.res = node.val
+#             return
+#         self.helper(node.right)
+              
+
+# Example 1:
+# Input: root = [3,1,4,null,2], k = 1
+# Output: 1
+    
+# Example 2:
+# Input: root = [5,3,6,2,4,null,null,1], k = 3
+# Output: 3
