d879423fcc71dccdd90670752ab9b1d5d09ddd81
Nick Anderson <nicasioca@gmail.com>
2022-04-25 23:56:15 -0700

    implement trie methods

--- 0208_implement_trie_prefix_tree.py
+++ 0208_implement_trie_prefix_tree.py
@@ -1,16 +1,34 @@
+class TrieNode:
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.is_word = False
+
 class Trie:
 
     def __init__(self):
+        self.root = TrieNode()
         
-
     def insert(self, word: str) -> None:
+        current = self.root
+        for letter in word:
+            current = current.children[letter]
+        current.is_word = True
         
-
     def search(self, word: str) -> bool:
-        
+        current = self.root
+        for letter in word:
+            current = current.children.get(letter)
+            if current == None:
+                return False
+        return current.is_word
 
     def startsWith(self, prefix: str) -> bool:
-        
+        current = self.root
+        for letter in prefix:
+            current = current.children.get(letter)
+            if current == None:
+                return False
+        return True
 
 
 # Your Trie object will be instantiated and called as such:
@@ -18,3 +36,19 @@ class Trie:
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
+# Example 1:
+# Input
+# ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+# [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+# Output
+# [null, null, true, false, true, null, true]
+
+# Explanation
+# Trie trie = new Trie();
+# trie.insert("apple");
+# trie.search("apple");   // return True
+# trie.search("app");     // return False
+# trie.startsWith("app"); // return True
+# trie.insert("app");
+# trie.search("app");     // return True
