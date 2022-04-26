e9ad3b9d389d0997bc68ff105aba1dae3a1deeff
Nick Anderson <nicasioca@gmail.com>
2022-04-25 23:19:12 -0700

    add lowest common ancestor using binary tree

--- 0235_lowest_common_ancestor_of_a_binary_search_tree.py
+++ 0235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -7,4 +7,41 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
+        if root == None or p == None or q == None:
+          return None
+
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
+            
+# Recursive
+# class Solution:
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+#         if root == None or p == None or q == None:
+#           return None
+
+#         if root.val > p.val and root.val > q.val:
+#             return self.lowestCommonAncestor(root.left, p, q)
+#         elif root.val < p.val and root.val < q.val:
+#             return self.lowestCommonAncestor(root.right, p, q)
+#         else:
+#             return root     
+
+
+# Example 1:
+# Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+# Output: 6
+# Explanation: The LCA of nodes 2 and 8 is 6.
+    
+# Example 2:
+# Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+# Output: 2
+# Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+    
+# Example 3:
+# Input: root = [2,1], p = 2, q = 1
+# Output: 2
