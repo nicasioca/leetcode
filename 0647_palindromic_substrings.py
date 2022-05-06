f905aaa745ff98c4480e63d6fed0bc5bdb6cc514
Nick Anderson <nicasioca@gmail.com>
2022-05-06 00:58:44 -0700

    update logic to solution

--- 0647_palindromic_substrings.py
+++ 0647_palindromic_substrings.py
@@ -1,10 +1,10 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        size = len(s)
         count = 0
-        for center in range(2*size - 1):
-            l = int(center / 2)
-            r = l + center % 2
+        size = len(s)
+        for center in range(2*size):
+            l = int(center/2)
+            r = l + center%2
             while l >= 0 and r < size:
                 if s[l] != s[r]:
                     break
