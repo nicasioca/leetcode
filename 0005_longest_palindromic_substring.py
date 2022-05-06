2a2098f9e0cfc5a4e35ee119fe378be492f6e648
Nick Anderson <nicasioca@gmail.com>
2022-05-05 23:29:21 -0700

    make readable longest palindromic substring

--- 0005_longest_palindromic_substring.py
+++ 0005_longest_palindromic_substring.py
@@ -1,37 +1,38 @@
 class Solution:
-    def longest_palindrome(self, s: str) -> str:
+    def longestPalindrome(self, s):
+        res = ''
         
-        # Manacher algorithm
-        #http://en.wikipedia.org/wiki/Longest_palindromic_substring
-        # Transform S into T.
-        # For example, S = "abba", T = "^#a#b#b#a#$".
-        # ^ and $ signs are sentinels appended to each end to avoid bounds checking
-
-        T = '#'.join('^{}$'.format(s))
-        n = len(T)
-        P = [0] * n
-        C = R = 0
-        for i in range (1, n-1):
-
-            # As long as R is greater than i,
-            # set the minimum for P
-            P[i] = (R > i) and min(R - i, P[2*C - i]) # equals to i' = C - (i-C)
-
-            # Attempt to expand palindrome centered at i
-            while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
-                P[i] += 1
-
-            # If palindrome centered at i expand past R,
-            # adjust center based on expanded palindrome
-            if i + P[i] > R:
-                C, R = i, i + P[i]
-        
-        # Find the maximum element in P
-        max_len, center_index = max((n, i) for i, n in enumerate(P))
-        return s[ (center_index - max_len)//2 : (center_index + max_len)//2 ]
-
-
-if __name__ == '__main__':
-    s = Solution()
-    print(s.longest_palindrome("banana"))
+        for i in range(len(s)):
+            
+            # odd case like "aba"
+            tmp = self.helper(s, i, i)
+            res = res if len(res) > len(tmp) else tmp
+            
+            # even case like "abba"
+            tmp = self.helper(s, i, i+1)
+            res = res if len(res) > len(tmp) else tmp
+            
+        return res
     
+    # get the longest palindrome, l, r are the middle indexes   
+    # from inner to outer
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s):
+            if s[l] != s[r]:
+                break
+            l -= 1
+            r += 1
+            
+        # add back to left to be inclusive of the correct slice
+        # right is already one further right so leave as is
+        return s[l+1:r]
+
+
+# Example 1:
+# Input: s = "babad"
+# Output: "bab"
+# Explanation: "aba" is also a valid answer.
+
+# Example 2:
+# Input: s = "cbbd"
+# Output: "bb"
